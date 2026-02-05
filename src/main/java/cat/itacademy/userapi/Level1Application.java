bd7200e848095b75bea77d9ef31a653126ff4f38
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-02-05 20:51:28 +0100

    initial commit

---
+++ src/main/java/cat/itacademy/userapi/Level1Application.java
@@ -0,0 +1,13 @@
+package cat.itacademy.userapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Level1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Level1Application.class, args);
+    }
+
+}
